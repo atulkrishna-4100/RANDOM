da1dd05e164df5691ba8cbe7a50830fbc44e0935
atulkrishna-4100 <147036093+atulkrishna-4100@users.noreply.github.com>
2024-09-25 14:50:15 +0530

    Done

---
+++ 10 Advanced Features/feature5.py
@@ -0,0 +1,15 @@
+def my_decorator(func): 
+    def wrapper(*args, **kwargs): 
+        print("Before the function execution.")
+        result = func(*args, **kwargs)
+        print("After the function execution.")
+        return result
+    
+    return wrapper
+
+@my_decorator
+def say_hello(name): 
+    print(f"Hello, {name}")
+
+if __name__ == "__main__": 
+    say_hello("Atul")
