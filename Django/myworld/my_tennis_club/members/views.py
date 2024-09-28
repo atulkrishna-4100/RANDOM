c6aba2d12a8ed3cf45b4ba7f4923ed7438bb9f43
atulkrishna-4100 <147036093+atulkrishna-4100@users.noreply.github.com>
2024-09-28 14:08:14 +0530

    Done

--- Django/myworld/my_tennis_club/members/views.py
+++ Django/myworld/my_tennis_club/members/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
-# Create your views here.
-def members(request): 
-    template = loader.get_template('members\myfirst.html')
-    return HttpResponse(template.render())
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('members/all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('members/details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
