fbb31da258bf214ebbabcc7915ed7e69fb7f06ec
atulkrishna-4100 <147036093+atulkrishna-4100@users.noreply.github.com>
2024-10-27 15:45:39 +0530

    Done

---
+++ ReactJS/Project01/Project01.js
@@ -0,0 +1,12 @@
+const page = (
+    <div>
+        <h1>Medical Store</h1>
+        <ol>
+            <li>Home</li>
+            <li>About</li>
+            <li>Pricing</li>
+        </ol>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
