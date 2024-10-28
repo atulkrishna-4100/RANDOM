a90d2ec4f17574067243776e255bbd961c7f6a00
atulkrishna-4100 <147036093+atulkrishna-4100@users.noreply.github.com>
2024-10-28 11:20:36 +0530

    Done

--- ReactJS/Project01/Project01.js
+++ ReactJS/Project01/Project01.js
@@ -1,12 +1,16 @@
-const page = (
-    <div>
-        <h1>Medical Store</h1>
-        <ol>
-            <li>Home</li>
-            <li>About</li>
-            <li>Pricing</li>
-        </ol>
-    </div>
-)
-
-ReactDOM.render(page, document.getElementById("root"))
+function TemporaryPage() {
+    return (
+        <div>
+            <img src="React_Logo.png" width="40px" placeholder="React-Logo"></img>
+            <h1>Fun facts about React: </h1>
+            <ul>
+                <li>Was first released in 2013.</li>
+                <li>Was originally created by Jordan Walke.</li>
+                <li>Has well over 100K stars on GitHub.</li>
+                <li>Is maintained by Facebook.</li>
+                <li>Powers thousands of enterprise apps, including mobile apps.</li>
+            </ul>
+        </div>
+    )
+}
+ReactDOM.render(<TemporaryPage />, document.getElementById("root"))
